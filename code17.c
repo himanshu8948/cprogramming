7350f6d2277f3243b0bd1a87181770adeb25707e
himanshu8948 <himanshupathak894820@gmail.com>
2026-06-10 10:14:22 +0530

    code17

---
+++ code17.c
@@ -0,0 +1,30 @@
+#include<stdio.h> 
+int main(){
+
+int a ;
+
+printf("Enter the year to check if it is leap year or not : ");
+scanf("%d",&a);
+
+if (a % 4 == 0){
+
+  if(a % 100 == 0){
+
+    if(a % 400 == 0){
+
+        printf("the input year is a leap year !:");
+    }
+    else {
+        printf("the inpur year is not a leap year !");
+    }
+}
+    else{
+
+        printf("the input year is a leap year ! :");
+    }}
+
+  else {
+     printf("the input year is not a leap year ! :");
+}
+return 0;
+}
