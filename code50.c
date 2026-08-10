316342af444a7e9f738c9c6cb20a654bd6e0dd79
himanshu8948 <himanshupathak894820@gmail.com>
2026-08-10 13:31:00 +0530

    code50

---
+++ code50.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void printtillN(int n){
+
+    if(n == 0){
+
+        return;
+    }
+    printtillN(n-1);
+    printf("%d",n);
+}
+
+   
+
+int main(){
+    
+    int n;
+    int result;
+    
+    printf("Enter the value of n till which you wanted to print n !");
+
+    scanf("%d",&n);
+
+    
+    printtillN(n);
+
+   return 0;
+}
